b8f595a0c43b75cc09c0de7b4ada4b8f81a4699c
ArnGro123 <98963288+ArnGro123@users.noreply.github.com>
2022-05-23 16:14:17 -0400

    Add files via upload

---
+++ angle.py
@@ -0,0 +1,5 @@
+import math
+
+coord = (3,3)
+
+print(math.degrees(math.atan2(coord[0],coord[1])))
--- game_mech.py
+++ game_mech.py
@@ -1,11 +1,16 @@
-from turtle import shape
-import pygame, sys
+import pygame, sys, math, random as rand
 
 pygame.init()
 
 display = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Pushaway Game Mechanic Testing")
 clock = pygame.time.Clock()
+
+sprite = pygame.image.load("concept_art_sprite.png").convert_alpha()
+sprite = pygame.transform.smoothscale(sprite,(60,100))
+
+sprite_2 = pygame.image.load("concept_art_sprite_2.png").convert_alpha()
+sprite_2 = pygame.transform.smoothscale(sprite_2,(80,20))
 
 background_color = (255,255,255) #white
 shape_coords = [250,250]
@@ -39,7 +44,25 @@ def direction_mech(mouse,shape):
         dr_y = 1
     
     return dr_x,dr_y
+
+def angle_calc(coord1,coord2):
+    angle = math.degrees(math.atan2(coord1,coord2)) 
+    return angle
+
+class Particle():
+    def __init__(self,x,y,image,direction):
+        self.x = x
+        self.y = y
+        self.circle = image
+        self.direction = direction
     
+    def spawn(self):
+        display.blit(self.circle,(self.x,self.y))
+
+    def particles_force(self):
+        force = rand.randrange(1,5)
+        self.x += force * self.direction
+        self.y += force * self.direction
 
 
 while True:
@@ -52,14 +75,23 @@ while True:
             mouse_coord = pygame.mouse.get_pos()
             dr_x,dr_y = direction_mech(mouse_coord,shape_coords)
             force = 5
-            velocity = 0 
+            velocity = 0
+            for i in range(10):
+                Particle.spawn(self)
+                Particle.particles_force(self)
 
 
     display.fill(background_color)
+    display.blit(sprite,(shape_coords[0],shape_coords[1]))
+    
+    rotated_sprite_2 = pygame.transform.rotate(sprite_2,angle_calc(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]))
+    sprite_2_rect = rotated_sprite_2.get_rect()
+    sprite_2_rect.center = (shape_coords[0],shape_coords[1]+40)
 
-    pygame.draw.circle(display,"blue",(shape_coords[0],shape_coords[1]),15)
+    display.blit(rotated_sprite_2,(shape_coords[0],shape_coords[1]+40))
 
-    if shape_coords[1] + 20 >= 500:
+
+    if shape_coords[1] + 100 >= 500:
         pass
     else:
         shape_coords = gravity(shape_coords[0],shape_coords[1])
@@ -73,6 +105,7 @@ while True:
         force -= 0.1
     
     print(force)
+
 
     pygame.display.update()
     clock.tick(30)
